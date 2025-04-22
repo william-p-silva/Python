db23575e36434d5c3b7a6481e80c77cd30b31dcd
william-p-silva <josepereirawilliam@gmail.com>
2025-04-22 20:14:56 -0300

    tuplas

--- python-curso-video/tuplas/tuplas.py
+++ python-curso-video/tuplas/tuplas.py
@@ -1,4 +1,19 @@
 lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata frita')
-c = 1
+# c = comida
 for c in range(0, len(lanche)):
-    print(f"Comi {lanche[c]}")
+    #caso eu queira mostrar o dado e a posição do dado 
+    print(f"Comi {lanche[c]} na posição {c}")
+
+print('\n')
+for c in lanche:
+    #caso só precise do dado sem a posição
+    print(f"Comi {c}")
+
+print('\n')
+
+for pos, c in enumerate(lanche):
+    #caso eu queira mostrar o dado e a posição do dado só que mais dificil 
+    print(f"Comi {c} na posição {pos}")
+print('\n')
+
+print(sorted(lanche))
