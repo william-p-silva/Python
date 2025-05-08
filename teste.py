6a6864a4599520aa12b09fb5948db670bd4fa4b3
william-p-silva <josepereirawilliam@gmail.com>
2025-05-08 16:21:13 -0300

    só brincando

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 def helloWord(f):
     print(f)
-    print(type(f))
+    
     for k, v in enumerate(f):
         print(f"{v}", end='')
 
@@ -9,4 +9,5 @@ dic = {'nome': "william", "idade": [12, 25, 65, 19]}
 
 
 helloWord(f"{dic}")
-
+helloWord("Ola a todos meus caros compatriotas de guerra")
+helloWord("O Capitão PT é um otimo personagem")
